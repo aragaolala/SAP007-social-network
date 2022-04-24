dc3d44c442ebcf88e7ccc779b43ece490ccfb79b
Layssa <oilayssaa@gmail.com>
2022-04-24 20:22:24 -0300

    painel compartilhar post

---
+++ src/componentes/home-mobile.js
@@ -0,0 +1,24 @@
+const painelCompartilhar = document.createElement('form');
+  painelCompartilhar.setAttribute('id', 'formCompartilhar');
+  painelCompartilhar.classList.add('painelCompartilhar');
+  painelCompartilhar.innerHTML = `
+    <textarea id="inputCompartilhar" placeholder="O que gostaria de compartilhar?" rows="8" cols="76"></textarea>  
+    
+    <div class="botoes">
+        <input type="file" placeholder="Adicionar imagem" id="compartilharImg">         
+        <select name="Grupo" id="Grupo" class="Grupo">
+          <option value="" selected disabled>Tema</option>
+          <option value="Relacionamentos">Relacionamento</option>
+          <option value="Web">Web</option>
+          <option value="Saúde">Saúde</option>
+          <option value="Viagens">Viagens</option>
+          <option value="Amizade">Amizade</option>
+          <option value="Moda">Moda/Beleza</option>
+          <option value="Estudos">Estudos</option>
+          <option value="Maternidade">Maternidade</option>
+          <option value="Outros">Outros</option>
+        </select>
+        <button class="botaoCompartilhar">Publicar</button>
+          
+    </div>
+  `;
