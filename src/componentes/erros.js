f9b7f390fcf9728d54af38e158932e0015d6e5b3
Layssa <oilayssaa@gmail.com>
2022-04-24 17:08:04 -0300

    formantando doc

--- src/componentes/erros.js
+++ src/componentes/erros.js
@@ -1,49 +1,48 @@
 //  Objeto que cria de forma dinâmica os modos para Registro
 
 export const funcRegistro = {
-
-    emailInvalido: () => {
-      const invalidoEmailMsg = `
+  emailInvalido: () => {
+    const invalidoEmailMsg = `
         <div class= "exibicaoError" id="exibicaoemailInvalido">
           <i class="fas fa-exclamation-triangle"></i>
           <p>E-mail inválido</p>
         </div>
       `;
-      return invalidoEmailMsg;
-    },
-  
-    emailExistente: () => {
-      const msgEmailJaExistente = `
+    return invalidoEmailMsg;
+  },
+
+  emailExistente: () => {
+    const msgEmailJaExistente = `
         <div class= "exibicaoError" id="exibicaoEmailExistente">
           <i class="fas fa-check-circle"></i>
           <p>Esse e-mail já existe</p>
         </div>
       `;
-      return msgEmailJaExistente;
-    },
-  
-    senhaInvalida: () => {
-      const msgSenhaFraca = `
+    return msgEmailJaExistente;
+  },
+
+  senhaInvalida: () => {
+    const msgSenhaFraca = `
         <div class= "exibicaoError" id="exibicaoSenhaInvalida">
           <i class="fas fa-check-circle"></i>
           <p>A senha deve conter pelo menos 6 caracteres</p>
         </div>
       `;
-      return msgSenhaFraca;
-    },
-    
-    registroSucesso: () => {
-        const registroSucessoMensagem = `
+    return msgSenhaFraca;
+  },
+
+  registroSucesso: () => {
+    const registroSucessoMensagem = `
           <div class= "exibicaoRegistroSucesso" id="exibicaoRegistroSucesso">
             <i class="fas fa-check-circle"></i>
             <p>Verifique seu e-mail para confirmar sua conta</p>
           </div>
         `;
-        return registroSucessoMensagem;
-      },
-    };
+    return registroSucessoMensagem;
+  },
+};
 
-    // Objeto que cria de forma dinâmica os modos para Início de Sessão
+// Objeto que cria de forma dinâmica os modos para Início de Sessão
 export const modoInicioSecao = {
   confirmar: () => {
     const confirmeSeuEmailMsg = `
