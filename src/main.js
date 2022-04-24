e2781ca333f8f72428452186f644b1f077a7ba3f
Layssa <oilayssaa@gmail.com>
2022-04-24 13:12:27 -0300

    pasta para as rotas

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // Este es el punto de entrada de tu aplicacion
 
 import { myFunction } from "./lib/index.js";
-import { routes } from "./lib/routes.js";
+import { routes } from "./router/routes.js";
 
 myFunction();
 const init = () => {
