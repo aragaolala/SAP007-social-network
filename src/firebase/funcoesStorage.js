20170a860ab02fe30ca28743ad8834d870dcf6a1
Layssa <oilayssaa@gmail.com>
2022-04-24 20:29:30 -0300

    funcao para subir dados no storage

--- src/firebase/funcoesStorage.js
+++ src/firebase/funcoesStorage.js
@@ -1,4 +1,18 @@
 import {
-    storage,
-    ref
-  } from './config.js';
+  storage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} from './config.js';
+
+// Faz o upload do arquivo para o storage e devolve o url da imagem
+export const subirFileStorage = async (file, pasta) => {
+  if (!file) {
+    return null;
+  }
+
+  const arquivoRef = ref(storage, `${pasta}/${file.name}`);
+  await uploadBytes(arquivoRef, file);
+  const trazerArquivo = getDownloadURL(arquivoRef);
+  return trazerArquivo;
+};
