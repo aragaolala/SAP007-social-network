09004ffa16c79e8e3c48b1617335a74b5d181eab
Layssa <oilayssaa@gmail.com>
2022-04-24 14:02:06 -0300

    obtendo dados do user pelo id no firestore

--- src/firebase/funcoesFirestore.js
+++ src/firebase/funcoesFirestore.js
@@ -1,28 +1,43 @@
 import {
-    db,
-    collection,
-    getDocs,
-    getDoc,
-    doc,
-    setDoc,
-    addDoc,
-    updateDoc,
-    query,
-    orderBy,
-    serverTimestamp,
-    deleteDoc,
-    onSnapshot,
-  } from './config.js';
+  db,
+  collection,
+  getDocs,
+  getDoc,
+  doc,
+  setDoc,
+  addDoc,
+  updateDoc,
+  query,
+  orderBy,
+  serverTimestamp,
+  deleteDoc,
+  onSnapshot,
+} from "./config.js";
 
-  // Adicionar dados iniciais para o momento de registro para a coleção de usuários
-export const adicionarDataUserFS = async (id, Username, email, Pronomes, Local, srcImg, srcImgCapa) => {
-    const colRefId = doc(db, 'usuarios', id);
-    await setDoc(colRefId, {
-      username: Username,
-      email: email,
-      local: Local,
-      pronomes: Pronomes,
-      imgUsuario: srcImg,
-      imgCapa: srcImgCapa,
-    });
-  };
+// Obter os dados de cada usuario salvo no Firestore, procurar por id
+export const obterPeloId = (idUser, nameColeccion) => {
+  const docRef = doc(db, nameColeccion, idUser);
+  const querySnapshot = getDoc(docRef).then((docs) => docs.data());
+  return querySnapshot;
+};
+
+// Adicionar dados iniciais para o momento de registro para a coleção de usuários
+export const adicionarDataUserFS = async (
+  id,
+  Username,
+  email,
+  Pronomes,
+  Local,
+  srcImg,
+  srcImgCapa
+) => {
+  const colRefId = doc(db, "usuarios", id);
+  await setDoc(colRefId, {
+    username: Username,
+    email: email,
+    local: Local,
+    pronomes: Pronomes,
+    imgUsuario: srcImg,
+    imgCapa: srcImgCapa,
+  });
+};
