af7f6915bec93ee2196b600e465829f60dc4331d
Layssa <oilayssaa@gmail.com>
2022-04-25 11:11:55 -0300

    funcao handler do like

--- src/componentes/categorias.js
+++ src/componentes/categorias.js
@@ -6,3 +6,8 @@ import {
     subirLikes,
   } from "../firebase/funcoesFirestore.js";
   import { validarSessaoStorage } from "./validacoes.js";
+
+  // Handler: Ações que devem ser executadas quando o usuário clicar em um botão
+async function LikeFunctionHandler(e) {
+    const btnLike = e.target;
+    const userData = JSON.parse(sessionStorage.userSession);
