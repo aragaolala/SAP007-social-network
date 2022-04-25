efce0690f110a209fc890ad4360c64231e110aa9
Layssa <oilayssaa@gmail.com>
2022-04-25 12:03:55 -0300

    seção preenchida conforme dados do user

--- src/componentes/secao-perfil.js
+++ src/componentes/secao-perfil.js
@@ -3,3 +3,9 @@ import {
   } from '../firebase/funcoesFirestore.js';
   import { btnLikes1 } from './categorias.js';
   import { validarSessaoStorage } from './validacoes.js';
+
+  // Render do post conforme usuario
+const postsUsuario = (idPost, dataCriador, dataPost) => {
+    const divPost = document.createElement('div');
+    divPost.classList.add('painelPost');
+  
