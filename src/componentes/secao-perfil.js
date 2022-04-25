ef88db976eb7a28bd8da694fbfb2a873eb29491d
Layssa <oilayssaa@gmail.com>
2022-04-25 12:02:57 -0300

    imports iniciais

---
+++ src/componentes/secao-perfil.js
@@ -0,0 +1,5 @@
+import {
+    obterUserPosts, eliminarPost, atualizarPost, obterUsuarios,
+  } from '../firebase/funcoesFirestore.js';
+  import { btnLikes1 } from './categorias.js';
+  import { validarSessaoStorage } from './validacoes.js';
