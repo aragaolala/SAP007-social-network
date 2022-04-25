554c7c932aebc57557b3e837b589af83888bc9db
Layssa <oilayssaa@gmail.com>
2022-04-25 11:32:39 -0300

    exportando const da versao desktop

--- src/componentes/home-desktop.js
+++ src/componentes/home-desktop.js
@@ -1,1 +1,6 @@
 import { validarSessaoStorage } from './validacoes.js';
+
+// Renderização da seção de dados do usuário e categorias de versão desktop
+export const homeParaDesktop = () => {
+    const secaoDesktop = document.createElement('section');
+    secaoDesktop.classList.add('item2');
