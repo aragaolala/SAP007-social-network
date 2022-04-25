68213c68cb20893efed4d5cea86b7634f6ab72a3
Layssa <oilayssaa@gmail.com>
2022-04-24 22:05:35 -0300

    imports 2

--- src/componentes/categorias.js
+++ src/componentes/categorias.js
@@ -1,1 +1,8 @@
 import { mostrarPost } from "./home-mobile.js";
+import {
+    obterPostsGrupo,
+    obterUsuarios,
+    obterPeloId,
+    subirLikes,
+  } from "../firebase/funcoesFirestore.js";
+  import { validarSessaoStorage } from "./validacoes.js";
