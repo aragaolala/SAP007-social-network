6d3a3a3df9e87d3dc4240c07693794240a45a780
Layssa <oilayssaa@gmail.com>
2022-04-25 12:04:30 -0300

    add template string

--- src/componentes/secao-perfil.js
+++ src/componentes/secao-perfil.js
@@ -8,4 +8,30 @@ import {
 const postsUsuario = (idPost, dataCriador, dataPost) => {
     const divPost = document.createElement('div');
     divPost.classList.add('painelPost');
+
+    divPost.innerHTML = `
+    <div class="usuarioPost" id="${idPost}">
+        <div class="imgUsuarioPost"><img class="imgPost"src="${dataCriador.imgUsuario}"></div>
+        <div class="infoUsuarioPost">
+            <div class="nomeUsuarioPost"><p>${dataCriador.username}</p></div>
+            <div class="descricaoUsuarioPost"><p>${dataCriador.pronomes}</p></div>
+        </div>
+        <button class="btnEdit"><img src="imagens/edit.png"></button>
+        <button class="btnDelete"><img src="imagens/delete.png"></button>
+    </div>
+    <div class="postsCompartilhado">
+        <div class="conteudoCompartilhado">
+            <p class="postcontent">${dataPost.publicacao }</p>
+            <img src="${dataPost.imgPost}">
+        </div>
+    </div>
+    <div class="botoesReacao">
+        <i class="ph-heart-bold like" name= "${idPost}"}></i>
+        <p>${dataPost.likes.length}</p>
+        <i class="ph-chat-circle" name= "${idPost}"}></i>
+        <p>${dataPost.likes.length}</p>        
+    </div>
+    `;
+  return divPost;
+};
   
