6ae68471bdb220a07343459963b60988bd136ae9
Layssa <oilayssaa@gmail.com>
2022-04-25 11:57:46 -0300

    formatando docs

--- src/componentes/timeline.js
+++ src/componentes/timeline.js
@@ -1,26 +1,26 @@
-import { conteudoHeader, secaoDeExibicao } from './headerFeed.js';
-import { homeParaDesktop } from './home-desktop.js';
-import { homeMobile } from './home-mobile.js';
-import { conteudoPerfil } from './secao-perfil.js';
-import { conteudoEditarPerfil } from './editar-perfil.js';
-import { conteudoCategoria } from './categorias.js';
+import { conteudoHeader, secaoDeExibicao } from "./headerFeed.js";
+import { homeParaDesktop } from "./home-desktop.js";
+import { homeMobile } from "./home-mobile.js";
+import { conteudoPerfil } from "./secao-perfil.js";
+import { conteudoEditarPerfil } from "./editar-perfil.js";
+import { conteudoCategoria } from "./categorias.js";
 
 // Render da seção timeline
 export const painel = () => {
-    const articlePainel = document.createElement('article');
-    articlePainel.classList.add('artPainel');
-    articlePainel.setAttribute('id', 'artPainel');
+  const articlePainel = document.createElement("article");
+  articlePainel.classList.add("artPainel");
+  articlePainel.setAttribute("id", "artPainel");
 
-    const headerFeed = document.createElement('header');
-  headerFeed.classList.add('item1');
+  const headerFeed = document.createElement("header");
+  headerFeed.classList.add("item1");
   headerFeed.innerHTML = conteudoHeader();
 
-  const secaoExibicaoCategoria = document.createElement('div');
-  secaoExibicaoCategoria.classList.add('exibicaoCategoria');
+  const secaoExibicaoCategoria = document.createElement("div");
+  secaoExibicaoCategoria.classList.add("exibicaoCategoria");
   secaoExibicaoCategoria.innerHTML = secaoDeExibicao();
 
-  const divSecoes = document.createElement('div');
-  divSecoes.classList.add('grid-container');
+  const divSecoes = document.createElement("div");
+  divSecoes.classList.add("grid-container");
   divSecoes.appendChild(homeParaDesktop());
   divSecoes.appendChild(homeMobile());
 
@@ -28,9 +28,9 @@ export const painel = () => {
   articlePainel.appendChild(secaoExibicaoCategoria);
   articlePainel.appendChild(divSecoes);
 
-  const footer = document.createElement('footer');
-  footer.classList.add('footerDesktop');
-  footer.innerHTML = 'By: Amanda Gusmão e Layssa Aragão';
+  const footer = document.createElement("footer");
+  footer.classList.add("footerDesktop");
+  footer.innerHTML = "By: Amanda Gusmão e Layssa Aragão";
 
   articlePainel.appendChild(footer);
   return articlePainel;
