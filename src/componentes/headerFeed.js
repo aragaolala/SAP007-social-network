16cd51f2bd5da5918f6d573c3b2ec305ef4715ed
Layssa <oilayssaa@gmail.com>
2022-04-24 21:43:43 -0300

    add templatr string

--- src/componentes/headerFeed.js
+++ src/componentes/headerFeed.js
@@ -1,1 +1,24 @@
 import { encerrarAtividadeUsuario } from "../firebase/funcoesAuth.js";
+
+// Renderizando o header
+export const conteudoHeader = () => {
+    // const userData = validarSessaoStorage();
+    const headerFeed = `
+          <div class="titulo-header">
+              <img src="imagens/logo.png" id="Logo">
+          </div>
+          <div class="pontosVerticais">
+            <figure></figure>
+            <figure class="middle"></figure>
+            <p class="sair"></p>
+            <figure></figure>
+            <ul class="dropDown">
+              <li><a href='#/timeline'><img id="img-encerrar" src="imagens/home.png"><span>Início</span></a></li>
+              <li><a href='#/meuperfil'><img id="img-encerrar" src="imagens/do-utilizador.png"><span>Meu Perfil</span></a></li>
+              <li><a href='#/editarperfil'><img id="img-encerrar" src="imagens/configuracoes.png"><span>Configurações</span></a></li>
+              <li><a id="encerrar-sessao"><img id="img-encerrar" src="imagens/sign-out.png"><span>Sair</span></a></li>
+            </ul>
+          </div>
+        `;
+    return headerFeed;
+  };
