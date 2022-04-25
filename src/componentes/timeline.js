6071a4462b0b93f017179c09ba8a9fbf3914185f
Layssa <oilayssaa@gmail.com>
2022-04-25 11:51:37 -0300

    imports iniciais da timeline

--- src/componentes/timeline.js
+++ src/componentes/timeline.js
@@ -1,3 +1,6 @@
 import { conteudoHeader, secaoDeExibicao } from './headerFeed.js';
 import { homeParaDesktop } from './home-desktop.js';
 import { homeMobile } from './home-mobile.js';
+import { conteudoPerfil } from './secao-perfil.js';
+import { conteudoEditarPerfil } from './editar-perfil.js';
+import { conteudoCategoria } from './categorias.js';
