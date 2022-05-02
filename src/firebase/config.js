55e109d5e96ce986c9cbbdfe0b3da8aea28c1576
Layssa <oilayssaa@gmail.com>
2022-05-02 13:00:04 -0300

    eslint ok

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,7 +1,6 @@
 // Configuração do Firebase
 /* eslint-disable*/
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js';
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.9/firebase-app.js'; 
 
 import {
   onAuthStateChanged,
